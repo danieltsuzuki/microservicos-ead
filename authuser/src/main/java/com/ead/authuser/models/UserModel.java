1cf1d080245e089290b39486f6670c4883b4e39b
Daniel <massao.tsuzuki@gmail.com>
2024-06-09 22:54:22 -0300

    REFACTOR: userId

--- authuser/src/main/java/com/ead/authuser/models/UserModel.java
+++ authuser/src/main/java/com/ead/authuser/models/UserModel.java
@@ -21,7 +21,7 @@ public class UserModel implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID userID;
+    private UUID userId;
 
     @Column(nullable = false, unique = true, length = 50)
     private String username;
