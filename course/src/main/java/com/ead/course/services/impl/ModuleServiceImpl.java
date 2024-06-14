43831ca232d2db573bf30afa3f08cdd67baa6cc3
Daniel <massao.tsuzuki@gmail.com>
2024-06-14 00:47:55 -0300

    ADD: api module

--- course/src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
+++ course/src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
@@ -1,9 +1,17 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
+import com.ead.course.repositories.LessonRepository;
 import com.ead.course.repositories.ModuleRepository;
 import com.ead.course.services.ModuleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class ModuleServiceImpl implements ModuleService {
@@ -11,4 +19,34 @@ public class ModuleServiceImpl implements ModuleService {
     @Autowired
     private ModuleRepository moduleRepository;
 
+    @Autowired
+    private LessonRepository lessonRepository;
+
+    @Transactional
+    @Override
+    public void delete(ModuleModel moduleModel) {
+        List<LessonModel> lessonModelList = lessonRepository.findAllLessonsIntoModule(moduleModel.getModuleId());
+        if (!lessonModelList.isEmpty()) {
+            lessonRepository.deleteAll(lessonModelList);
+        }
+        moduleRepository.delete(moduleModel);
+    }
+
+    @Override
+    @Transactional
+    public ModuleModel save(ModuleModel moduleModel) {
+        return moduleRepository.save(moduleModel);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<ModuleModel> findModuleIntoCourse(UUID courseId, UUID moduleId) {
+        return moduleRepository.findModuleIntoCourse(courseId, moduleId);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<ModuleModel> findAllByCourse(UUID courseId) {
+        return moduleRepository.findAllModulesIntoCourse(courseId);
+    }
 }
