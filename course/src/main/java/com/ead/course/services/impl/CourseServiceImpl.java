c30526a884c296d00fe2300e8ab3995df4bd091a
Daniel <massao.tsuzuki@gmail.com>
2024-06-14 00:05:55 -0300

    ADD: api course

--- course/src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ course/src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -1,9 +1,19 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.models.CourseModel;
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
 import com.ead.course.repositories.CourseRepository;
+import com.ead.course.repositories.LessonRepository;
+import com.ead.course.repositories.ModuleRepository;
 import com.ead.course.services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class CourseServiceImpl implements CourseService {
@@ -11,4 +21,41 @@ public class CourseServiceImpl implements CourseService {
     @Autowired
     private CourseRepository courseRepository;
 
+    @Autowired
+    private ModuleRepository moduleRepository;
+
+    @Autowired
+    private LessonRepository lessonRepository;
+
+    @Transactional
+    @Override
+    public void delete(CourseModel courseModel) {
+        List<ModuleModel> moduleModelList = moduleRepository.findAllModulesIntoCourse(courseModel.getCourseID());
+        if(!moduleModelList.isEmpty()) {
+            for (ModuleModel module : moduleModelList) {
+                List<LessonModel> lessonModelList = lessonRepository.findAllLessonsIntoModule(module.getModuleId());
+                if (!lessonModelList.isEmpty()) {
+                    lessonRepository.deleteAll(lessonModelList);
+                }
+                moduleRepository.deleteAll(moduleModelList);
+            }
+        }
+        courseRepository.delete(courseModel);
+    }
+
+    @Override
+    @Transactional
+    public CourseModel save(CourseModel courseModel) {
+        return courseRepository.save(courseModel);
+    }
+
+    @Override
+    public Optional<CourseModel> findById(UUID courseId) {
+        return courseRepository.findById(courseId);
+    }
+
+    @Override
+    public List<CourseModel> findAll() {
+        return courseRepository.findAll();
+    }
 }
